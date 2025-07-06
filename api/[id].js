29487aecd3d81be3eabd5172cfb64bd2aebc1366
goodboys21 <akunceeingood5@gmail.com>
2025-07-06 21:54:45 +0700

    Update [id].js

--- api/[id].js
+++ api/[id].js
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch';
 
-const RAW_URL = 'https://raw.githubusercontent.com/codegood21/code/main/urls.json';
+const BIN_URL = 'https://jsonblob.com/api/jsonBlob/1391431496337907712';
 
 export default async function handler(req, res) {
   const { id } = req.query;
 
   try {
-    const dbRes = await fetch(RAW_URL);
-    const db = await dbRes.json();
+    const getRes = await fetch(BIN_URL);
+    const db = await getRes.json();
 
     if (!db[id]) return res.status(404).send('Short URL not found');
 
     res.writeHead(302, { Location: db[id] });
     res.end();
-  } catch {
-    res.status(500).send('Failed to fetch redirect database');
+  } catch (e) {
+    res.status(500).send('JSONBlob error: ' + e.message);
   }
 }
