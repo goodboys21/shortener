2c855431bd727cb9c385fb4074cf8c4c89177fda
goodboys21 <akunceeingood5@gmail.com>
2025-10-13 12:39:26 +0700

    Update [id].js

--- api/[id].js
+++ api/[id].js
@@ -1,12 +1,21 @@
 import fetch from 'node-fetch';
 
-const BIN_URL = 'https://jsonblob.com/api/jsonBlob/1391431496337907712';
+const CONFIG_URL = 'https://files.catbox.moe/k7cs72.json';
+const FILE_PATH = 'shorturl.json';
+
+async function getConfig() {
+  const res = await fetch(CONFIG_URL);
+  if (!res.ok) throw new Error('Failed to fetch GitHub config from Catbox');
+  return await res.json();
+}
 
 export default async function handler(req, res) {
   const { id } = req.query;
 
   try {
-    const getRes = await fetch(BIN_URL);
+    const { username, repo, branch } = await getConfig();
+    const url = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/${FILE_PATH}`;
+    const getRes = await fetch(url);
     const db = await getRes.json();
 
     if (!db[id]) return res.status(404).send('Short URL not found');
@@ -14,6 +23,6 @@ export default async function handler(req, res) {
     res.writeHead(302, { Location: db[id] });
     res.end();
   } catch (e) {
-    res.status(500).send('JSONBlob error: ' + e.message);
+    res.status(500).send('GitHub DB error: ' + e.message);
   }
 }
